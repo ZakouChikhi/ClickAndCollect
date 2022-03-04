69394ac070f4e6783f5a0487dda131e44ed0e324
Chikhi Ahmed Zakaria <ahmed-zakaria.chikhi@etu.univ-orleans.fr>
2022-03-04 11:04:20 +0100

    initial the catalogue service

---
+++ service_catalogue/src/main/java/com/orleans/univ/microservices/servicecatalogue/ServicecatalogueApplication.java
@@ -0,0 +1,13 @@
+package com.orleans.univ.microservices.servicecatalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicecatalogueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicecatalogueApplication.class, args);
+    }
+
+}
---
+++ service_catalogue/src/test/java/com/orleans/univ/microservices/servicecatalogue/ServicecatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orleans.univ.microservices.servicecatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicecatalogueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
